8a47ab3e65a41963280d53fa44df1244f190592b
ngumina17 <nicole.gumina17@gmail.com>
2020-02-18 11:48:46 -0600

    models are made

---
+++ fit/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    photo_url = models.TextField()
+    
+    def __str__(self):
+        return self.name
+
+class Meals(models.Model):
+    Meal_Type = models.TextField()
+    contents = models.TextField()
+    calories = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='meals')
+
+    def __str__(self):
+        return self.Meal_Type
+
+class Goals(models.Model):
+    contents = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='goals')
+
+    def __str__(self):
+        return self.contents
+
+class Workout(models.Model):
+    completed = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='workouts')
+    
+    def __str__(self):
+        return self.completed
